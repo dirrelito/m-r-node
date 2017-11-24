84a6ada196d641350909f30df0c4470ff4414da6
Ludvig Hult <ludvig.hult@acando.com>
2017-11-24 23:48:53 +0100

    renamed URIs and added delete route

--- CQRSServer/item.read.service.ts
+++ CQRSServer/item.read.service.ts
@@ -16,4 +16,17 @@ export class ItemReadModel {
             res.json(usr);
         }
     }
+
+    public static deactivateItem = (req, res: Response) => {
+        console.log(req.params);
+        const id = req.params.id;
+        const pos = DummyDb.db.findIndex(i => i.id === id);
+        console.log(pos);
+        if (pos !== -1) {
+            DummyDb.db.splice(pos, 1);
+            res.json("ok");
+        } else {
+            res.status(404).json("Could not find that user");
+        }
+    }
 }
--- CQRSServer/routes.ts
+++ CQRSServer/routes.ts
@@ -3,10 +3,26 @@ import {ItemReadModel} from "./item.read.service";
 
 export const router = express.Router();
 
-router.get("/items", (req, res) => {
+router.get("/", (req, res) => {
+    res.json({
+            title: "List of commands for this API",
+            commands: [
+
+                {command: "List items", url: "/InventoryItem", method: "GET", comment: "Lists all items"},
+                {command: "Find item details", url: "/InventoryItem/:id", method: "GET"},
+                {command: "Deactivate item", url: "/InventoryItem/:id", method: "DELETE"},
+            ],
+        });
+});
+
+router.get("/InventoryItem", (req, res) => {
     ItemReadModel.getItems(req, res);
 });
 
-router.get("/item/:id", (req, res) => {
+router.get("/InventoryItem/:id", (req, res) => {
     ItemReadModel.getItem(req, res);
+});
+
+router.delete("/InventoryItem/:id", (req, res) => {
+    ItemReadModel.deactivateItem(req, res);
 });
