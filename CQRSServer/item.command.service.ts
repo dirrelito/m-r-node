2ec0841e9b413222c4014f29b5d8251b98ab4daa
Ludvig Hult <ludvig.hult@acando.com>
2017-11-28 19:38:44 +0100

    cosmetic update

--- CQRSServer/item.command.service.ts
+++ CQRSServer/item.command.service.ts
@@ -72,6 +72,8 @@ export class ItemCommandService {
         const newName = req.body.name;
         const id = uuid();
         eventbus.Send(new CreateInventoryItem(id, newName));
-        res.location(`/api/IventoryItem/${id}`).status(202).end();
+        res.location(`/api/IventoryItem/${id}`)
+           .status(202)
+           .end();
     }
 }
